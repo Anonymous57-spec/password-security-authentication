d1284a732f9bfb8fb1c6d8f81800b3f86e87c6ea
Tarun Kushwaha <kushwahatarun67@gmail.com>
2026-04-06 23:32:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from password_checker import check_password_strength
+from auth_system import hash_password, verify_password
+
+print("=== Password Security System ===")
+
+password = input("Create password: ")
+print(check_password_strength(password))
+
+hashed = hash_password(password)
+
+login = input("Enter password: ")
+
+if verify_password(hashed, login):
+    print("Login Successful ✅")
+else:
+    print("Wrong Password ❌")
